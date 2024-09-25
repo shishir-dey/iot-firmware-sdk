66bcbdeee2fe2ea2fe1bdc1ea628c0e03cf1af63
Shishir Dey <shishir@bytebeam.io>
2024-09-25 12:30:31 +0530

    Add support for the data types that we have created

---
+++ include/bytebeam/custom_types.h
@@ -0,0 +1,25 @@
+#ifndef CUSTOM_TYPES_H
+#define CUSTOM_TYPES_H
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <time.h>
+
+typedef bool bb_bool_t;
+typedef float bb_float32_t;
+typedef double bb_float64_t;
+typedef int8_t bb_int8_t;
+typedef int16_t bb_int16_t;
+typedef int32_t bb_int32_t;
+typedef int64_t bb_int64_t;
+typedef uint8_t bb_uint8_t;
+typedef uint16_t bb_uint16_t;
+typedef uint32_t bb_uint32_t;
+typedef uint64_t bb_uint64_t;
+typedef char* bb_string_t;
+typedef int64_t bb_datetime64_t;
+typedef int64_t bb_timestamp_t;
+typedef int64_t bb_timestamp_ms_t;
+typedef int64_t bb_timestamp_us_t;
+
+#endif /* CUSTOM_TYPES_H */
