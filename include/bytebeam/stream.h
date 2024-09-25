578fd465f53cf8731f6d4b083c559e387b02efed
Shishir Dey <shishir@bytebeam.io>
2024-09-25 14:45:07 +0530

    Add support for a 'name' field to stream objects

--- include/bytebeam/stream.h
+++ include/bytebeam/stream.h
@@ -5,156 +5,121 @@
 extern "C" {
 #endif
 
-typedef cJSON stream;
-
 #include "bytebeam/custom_types.h"
 
+#define STREAM_NAME_MAX_LEN 32
+
+typedef struct
+{
+    cJSON* obj;
+    char name[STREAM_NAME_MAX_LEN];
+} stream;
+
 /**
- * @brief Creates a new stream object.
+ * @brief Creates a new stream object with the given name.
+ * @param name The name of the stream.
  * @return A pointer to the newly created stream object.
  */
-stream* stream_create();
+stream* stream_create(const char* name);
 
 /**
- * @brief Adds a boolean value to the stream object.
- * @param obj The stream object to add the value to.
- * @param key The key for the value.
- * @param value The boolean value to add.
+ * @brief Sets the name of an existing stream object.
+ * @param handle The stream object to set the name for.
+ * @param name The new name for the stream.
  */
-void stream_add_bool(stream* obj, const char* key, bb_bool_t value);
+void stream_set_name(stream* handle, const char* name);
 
 /**
- * @brief Adds a 32-bit float value to the stream object.
- * @param obj The stream object to add the value to.
- * @param key The key for the value.
- * @param value The 32-bit float value to add.
+ * @brief Gets the name of a stream object.
+ * @param handle The stream object to get the name from.
+ * @return The name of the stream.
  */
-void stream_add_float32(stream* obj, const char* key, bb_float32_t value);
+char* stream_get_name(stream* handle);
 
 /**
- * @brief Adds a 64-bit float value to the stream object.
- * @param obj The stream object to add the value to.
+ * @brief Adds an unsigned 8-bit integer value to the stream object.
+ * @param handle The stream object to add the value to.
  * @param key The key for the value.
- * @param value The 64-bit float value to add.
+ * @param value The unsigned 8-bit integer value to add.
  */
-void stream_add_float64(stream* obj, const char* key, bb_float64_t value);
+void stream_add_uint8(stream* handle, const char* key, bb_uint8_t value);
 
 /**
- * @brief Adds an 8-bit integer value to the stream object.
- * @param obj The stream object to add the value to.
+ * @brief Adds an unsigned 16-bit integer value to the stream object.
+ * @param handle The stream object to add the value to.
  * @param key The key for the value.
- * @param value The 8-bit integer value to add.
+ * @param value The unsigned 16-bit integer value to add.
  */
-void stream_add_int8(stream* obj, const char* key, bb_int8_t value);
+void stream_add_uint16(stream* handle, const char* key, bb_uint16_t value);
 
 /**
- * @brief Adds a 16-bit integer value to the stream object.
- * @param obj The stream object to add the value to.
+ * @brief Adds an unsigned 32-bit integer value to the stream object.
+ * @param handle The stream object to add the value to.
  * @param key The key for the value.
- * @param value The 16-bit integer value to add.
+ * @param value The unsigned 32-bit integer value to add.
  */
-void stream_add_int16(stream* obj, const char* key, bb_int16_t value);
+void stream_add_uint32(stream* handle, const char* key, bb_uint32_t value);
 
 /**
- * @brief Adds a 32-bit integer value to the stream object.
- * @param obj The stream object to add the value to.
+ * @brief Adds an unsigned 64-bit integer value to the stream object.
+ * @param handle The stream object to add the value to.
  * @param key The key for the value.
- * @param value The 32-bit integer value to add.
+ * @param value The unsigned 64-bit integer value to add.
  */
-void stream_add_int32(stream* obj, const char* key, bb_int32_t value);
-
-/**
- * @brief Adds a 64-bit integer value to the stream object.
- * @param obj The stream object to add the value to.
- * @param key The key for the value.
- * @param value The 64-bit integer value to add.
- */
-void stream_add_int64(stream* obj, const char* key, bb_int64_t value);
-
-/**
- * @brief Adds an 8-bit unsigned integer value to the stream object.
- * @param obj The stream object to add the value to.
- * @param key The key for the value.
- * @param value The 8-bit unsigned integer value to add.
- */
-void stream_add_uint8(stream* obj, const char* key, bb_uint8_t value);
-
-/**
- * @brief Adds a 16-bit unsigned integer value to the stream object.
- * @param obj The stream object to add the value to.
- * @param key The key for the value.
- * @param value The 16-bit unsigned integer value to add.
- */
-void stream_add_uint16(stream* obj, const char* key, bb_uint16_t value);
-
-/**
- * @brief Adds a 32-bit unsigned integer value to the stream object.
- * @param obj The stream object to add the value to.
- * @param key The key for the value.
- * @param value The 32-bit unsigned integer value to add.
- */
-void stream_add_uint32(stream* obj, const char* key, bb_uint32_t value);
-
-/**
- * @brief Adds a 64-bit unsigned integer value to the stream object.
- * @param obj The stream object to add the value to.
- * @param key The key for the value.
- * @param value The 64-bit unsigned integer value to add.
- */
-void stream_add_uint64(stream* obj, const char* key, bb_uint64_t value);
+void stream_add_uint64(stream* handle, const char* key, bb_uint64_t value);
 
 /**
  * @brief Adds a string value to the stream object.
- * @param obj The stream object to add the value to.
+ * @param handle The stream object to add the value to.
  * @param key The key for the value.
  * @param value The string value to add.
  */
-void stream_add_string(stream* obj, const char* key, bb_string_t value);
+void stream_add_string(stream* handle, const char* key, bb_string_t value);
 
 /**
  * @brief Adds a datetime64 value to the stream object.
- * @param obj The stream object to add the value to.
+ * @param handle The stream object to add the value to.
  * @param key The key for the value.
  * @param value The datetime64 value to add.
  */
-void stream_add_datetime64(stream* obj, const char* key, bb_datetime64_t value);
+void stream_add_datetime64(stream* handle, const char* key, bb_datetime64_t value);
 
 /**
  * @brief Adds a timestamp value to the stream object.
- * @param obj The stream object to add the value to.
+ * @param handle The stream object to add the value to.
  * @param key The key for the value.
  * @param value The timestamp value to add.
  */
-void stream_add_timestamp(stream* obj, const char* key, bb_timestamp_t value);
+void stream_add_timestamp(stream* handle, const char* key, bb_timestamp_t value);
 
 /**
  * @brief Adds a timestamp in milliseconds to the stream object.
- * @param obj The stream object to add the value to.
+ * @param handle The stream object to add the value to.
  * @param key The key for the value.
  * @param value The timestamp value in milliseconds to add.
  */
-void stream_add_timestamp_ms(stream* obj, const char* key, bb_timestamp_ms_t value);
+void stream_add_timestamp_ms(stream* handle, const char* key, bb_timestamp_ms_t value);
 
 /**
  * @brief Adds a timestamp in microseconds to the stream object.
- * @param obj The stream object to add the value to.
+ * @param handle The stream object to add the value to.
  * @param key The key for the value.
  * @param value The timestamp value in microseconds to add.
  */
-void stream_add_timestamp_us(stream* obj, const char* key, bb_timestamp_us_t value);
+void stream_add_timestamp_us(stream* handle, const char* key, bb_timestamp_us_t value);
 
 /**
  * @brief Prints the stream object as a JSON string.
- * @param obj The stream object to print.
- * @return A pointer to the JSON string representation of the stream object.
+ * @param handle The stream object to print.
+ * @return A JSON string representation of the stream object.
  */
-char* stream_print(stream* obj);
+char* stream_print(stream* handle);
 
 /**
- * @brief Deletes the stream object and frees associated memory.
- * @param obj The stream object to delete.
+ * @brief Deletes a stream object and frees associated memory.
+ * @param handle The stream object to delete.
  */
-void stream_delete(stream* obj);
+void stream_delete(stream* handle);
 
 #ifdef __cplusplus
 }
