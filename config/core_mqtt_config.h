31ed05377918500ef52b6deee14e05bf525038cc
Shishir Dey <shishir@bytebeam.io>
2024-09-30 12:30:32 +0530

    Remove coreMQTT logs

--- config/core_mqtt_config.h
+++ config/core_mqtt_config.h
@@ -1,12 +1,6 @@
 #ifndef CORE_MQTT_CONFIG_H
 #define CORE_MQTT_CONFIG_H
 
-#include <stdio.h>
-
-#define LogError(message) printf("Error: %s\n", message)
-#define LogWarn(message) printf("Warning: %s\n", message)
-#define LogInfo(message) printf("Info: %s\n", message)
-
 #define MQTT_STATE_ARRAY_MAX_COUNT (10U)
 
 #define MQTT_MAX_CONNACK_RECEIVE_RETRY_COUNT (2U)
